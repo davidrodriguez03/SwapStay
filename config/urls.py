2d2ba144a4571160e6f446e3df488c14e7972e95
Luis Agudelo <laagudelor@eafit.edu.co>
2026-02-27 14:57:58 -0500

    Actualizar config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from config.views import home 
 
 urlpatterns = [
-    path('', home), 
     path('admin/', admin.site.urls),
+    path('api/', include('reservas.api_urls')),  
     path('reservas/', include('reservas.urls')),
 ]
